a0be940cc55a89eff50f34c7ab89900823cde7d1
Samsal707 <samsal707@gmail.com>
2022-12-08 16:01:07 +1100

    first commit - project done to understand the flow of routes and role of controllers

--- meadowlark.js
+++ meadowlark.js
@@ -1,48 +1,71 @@
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
+
 
 const port = process.env.PORT || 3000
 
+const fortune = require('./lib/fortune')
+const requiresWaiver = require('./lib/tourRequiresWaiver')
+const cartValidation = require('./lib/cartValidation')
+
 const app = express()
 
+app.use(requiresWaiver)
 
+app.use(cartValidation.resetValidation)
+app.use(cartValidation.checkWaivers)
+app.use(cartValidation.checkGuestCounts)
 
-const fortunes = [
-  "Conquer your fears or they will conquer you.",
-  "Rivers need springs.",
-  "Do not fear what you don't know.",
-  "You will have a pleasant surprise.",
-  "Whenever possible, keep it simple.",
-]
-
-app.use(express.static(__dirname + '/public'))
-
-// configure Handlebars view engine
-app.engine('handlebars', expressHandlebars.engine({
-  defaultLayout: 'main',
-}))
-
-app.set('view engine', 'handlebars')
-
-app.get('/', (req, res) => res.render('home'))
-
-app.get('/about', (req, res) => {
-  const randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)]
-  res.render('about', { fortune: randomFortune })
+app.use((req, res, next) => { 
+	console.log('\n\nALLWAYS')
+	next() 
 })
 
-// custom 404 page
-app.use((req, res) => {
-  res.status(404)
-  res.render('404')
+app.get('/a', (req, res) => { 
+	console.log('/a: route terminated')
+	res.send('a')
+})
+app.get('/a', (req, res) => { 
+	console.log('/a: never called');
+})
+app.get('/b', (req, res, next) => { 
+	console.log('/b: route not terminated')
+	next()
+})
+app.use((req, res, next) => {
+	console.log('SOMETIMES')
+	next()
+})
+app.get('/b', (req, res, next) => {
+	console.log('/b (part 2): error thrown' )
+	throw new Error('b failed')
+})
+app.use('/b', (err, req, res, next) => {
+	console.log('/b error detected and passed on')
+	next(err)
+})
+app.get('/c', (err, req) => {
+	console.log('/c: error thrown')
+	throw new Error('c failed')
+})
+app.use('/c', (err, req, res, next) => {
+	console.log('/c: error detected but not passed on')
+	next()
 })
 
-// custom 500 page
 app.use((err, req, res, next) => {
-  console.error(err.message)
-  res.status(500)
-  res.render('500')
+	console.log('unhandled error detected: ' + err.message)
+	res.send('500 - server error')
 })
+
+app.use((req, res) => {
+	console.log('route not handled')
+	res.send('404 - not found')
+})
+
+
+
 
 app.listen(port, () => console.log(
   `Express started on http://localhost:${port}; ` +
